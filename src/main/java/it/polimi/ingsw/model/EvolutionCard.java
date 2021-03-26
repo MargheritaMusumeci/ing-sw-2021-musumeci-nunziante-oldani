21eb777cdcd037c2e07a2834777802831790249d
matteoldani <matte.oldani99@gmail.com>
2021-03-26 17:55:13 +0100

    Added EvolutionSection method to populate the section

--- src/main/java/it/polimi/ingsw/model/EvolutionCard.java
+++ src/main/java/it/polimi/ingsw/model/EvolutionCard.java
@@ -5,10 +5,9 @@ public class EvolutionCard {
     private CardColor color;
     private LevelEnum level;
     private int point;
-    private int cost;
-    private Resource costType;
-    private int[] requires; //rock, shield, coins, servants
-    private int[] product; //rock, shield, coins, servants, faith
+    private int[] cost; //rock, shield, coins, servant
+    private int[] requires; //rock, shield, coin, servant
+    private int[] product; //rock, shield, coin, servant, faith
     private boolean isActive;
 
     /**
@@ -17,16 +16,14 @@ public class EvolutionCard {
      * @param level represent the card level
      * @param point represent the card point
      * @param cost  represent the card cost
-     * @param costType represent the resource type of the cost
      * @param requires array of resources required to activate the card in the order rock, shield, coins, servants
      * @param product   array of the resource produced by the productioon in the order rock, shield, coins, servants, faith
      */
 
-    public EvolutionCard(CardColor color, LevelEnum level, int point, int cost, Resource costType, int[] requires, int[] product){
+    public EvolutionCard(CardColor color, LevelEnum level, int point, int[] cost, int[] requires, int[] product){
         this.color = color;
         this.level = level;
         this.point = point;
-        this.costType = costType;
         this.requires = requires;
         this.product = product;
         isActive = false;
@@ -44,12 +41,8 @@ public class EvolutionCard {
         return point;
     }
 
-    public int getCost() {
+    public int[] getCost() {
         return cost;
-    }
-
-    public Resource getCostType() {
-        return costType;
     }
 
     public int[] getRequires() {
