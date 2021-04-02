8d83f4fa02f703d134ba10288009af0e9dd9c3d3
matteoldani <matte.oldani99@gmail.com>
2021-04-02 22:18:58 +0200

    Created LeaderCardSet and its methods

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,2 +1,15 @@
-package it.polimi.ingsw.model;public class Game {
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Player> players;
+    private Market market;
+    private EvolutionSection evolutionSection;
+    private Player activePlayer;
+    private boolean isSoloGame;
+
+    public Game(ArrayList<String> playersNickname, )
+
 }
--- src/main/java/it/polimi/ingsw/model/LeaderCardSet.java
+++ src/main/java/it/polimi/ingsw/model/LeaderCardSet.java
@@ -1,2 +1,34 @@
-package it.polimi.ingsw.model;public class LeaderCardSet {
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeaderCardSet {
+
+    private ArrayList<LeaderCard> leaderCardSet;
+
+    public LeaderCardSet(){
+        leaderCardSet = new ArrayList<LeaderCard>();
+        //build the deck with the gson library
+        //instanzia le 16 carte leader e fa lo shuffle tanto lo fa una volta sola
+    }
+
+    /**
+     *
+     * @return the entire set
+     */
+    public ArrayList<LeaderCard> getLeaderCardSet() {
+        return (ArrayList<LeaderCard>) leaderCardSet.clone();
+    }
+
+    /**
+     *
+     * @param position of the card to be returned
+     * @return the card in the set at the given position
+     */
+    public LeaderCard getLeaderCard(int position){
+        //possibile esposizione del rep
+        return leaderCardSet.get(position);
+    }
+
 }
