a2fc68806e9c14c785ef881d98e14b2c9a6d01d2
Margherita Musumeci <marghe.musumeci@gmail.com>
2021-04-09 13:21:13 +0200

    Added serialization to Enum

--- src/main/java/it/polimi/ingsw/model/LorenzoAction.java
+++ src/main/java/it/polimi/ingsw/model/LorenzoAction.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Possible Solo Actions
  */
 public enum LorenzoAction {
+    @SerializedName("1")
     DISCARDEVOLUTION,
+    @SerializedName("2")
     INCREMENTPOPETRACK
 }
