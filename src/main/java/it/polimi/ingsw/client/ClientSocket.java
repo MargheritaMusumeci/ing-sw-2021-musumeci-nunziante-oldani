a6de2f0da1b37231b72b75e6848450edd33105b1
matteoldani <matte.oldani99@gmail.com>
2021-05-07 11:20:53 +0200

    Created logic for handling the connection of the client to the server

--- src/main/java/it/polimi/ingsw/client/ClientSocket.java
+++ src/main/java/it/polimi/ingsw/client/ClientSocket.java
@@ -1,4 +1,70 @@
 package it.polimi.ingsw.client;
 
-public class ClientSocket {
+import it.polimi.ingsw.client.CLI.CLI;
+import it.polimi.ingsw.messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientSocket implements Runnable{
+
+    private CLI cli;
+    private Socket socket;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    private MessageHandler messageHandler;
+    boolean isActive;
+
+    public ClientSocket(CLI cli, Socket socket) throws IOException {
+        this.cli = cli;
+        this.socket = socket;
+
+        inputStream = new ObjectInputStream(socket.getInputStream());
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+
+        messageHandler = new MessageHandler(cli);
+
+        isActive = true;
+
+    }
+
+    /**
+     * method that send the message to the server
+     * @param message is the message that has to be sent
+     */
+    public void send(Message message){
+        try {
+            outputStream.reset();
+            outputStream.writeObject(message);
+            outputStream.flush();
+        } catch (IOException e) {
+            close();
+        }
+    }
+
+    public void close(){
+        //Network error, handle this
+        isActive = false;
+    }
+
+
+    @Override
+    public void run() {
+        while(isActive){
+            System.out.println("inizio con il scc");
+            //leggo i messaggi in arrivo e li eseguo
+            try {
+                Message input = (Message) inputStream.readObject();
+                System.out.println("Messaggio letto: " + input.getMessage());
+                messageHandler.handleMessage(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
