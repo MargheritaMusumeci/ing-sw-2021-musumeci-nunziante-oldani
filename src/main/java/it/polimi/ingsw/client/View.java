be68550072672e2379b74f539b411416a11e36ee
matteoldani <matte.oldani99@gmail.com>
2021-05-13 15:59:02 +0200

    Implemented data structures to handle enemyLeaderCards

--- src/main/java/it/polimi/ingsw/client/View.java
+++ src/main/java/it/polimi/ingsw/client/View.java
@@ -2,10 +2,12 @@ package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.model.board.Dashboard;
 import it.polimi.ingsw.model.cards.LeaderCard;
+import it.polimi.ingsw.model.game.Resource;
 import it.polimi.ingsw.serializableModel.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class View implements Serializable {
 
@@ -13,10 +15,12 @@ public class View implements Serializable {
     private String nickname;
     private SerializableDashboard dashboard;
     private ArrayList<SerializableDashboard> enemiesDashboard;
+    private HashMap<SerializableDashboard, ArrayList<SerializableLeaderCard>> enemiesActivatedLeaderCards;
     private SerializableMarket market;
     private SerializableEvolutionSection evolutionSection;
     private ArrayList<SerializableLeaderCard> leaderCards;
     private int score;
+    private ArrayList<Resource> resourcesBoughtFromMarker;
 
     public View(String activePlayer, String nickname, SerializableDashboard dashboard, ArrayList<SerializableDashboard> enemiesDashboard,
                 SerializableMarket market, SerializableEvolutionSection evolutionSection, ArrayList<SerializableLeaderCard> leaderCards, int score) {
@@ -28,6 +32,12 @@ public class View implements Serializable {
         this.evolutionSection = evolutionSection;
         this.leaderCards = leaderCards;
         this.score = score;
+        resourcesBoughtFromMarker = new ArrayList<>();
+        enemiesActivatedLeaderCards = new HashMap<>();
+        for (SerializableDashboard enemyDashboard :
+                enemiesDashboard) {
+            enemiesActivatedLeaderCards.put(enemyDashboard, new ArrayList<>());
+        }
     }
 
     public String getActivePlayer() {
@@ -88,5 +98,13 @@ public class View implements Serializable {
 
     public void setEnemiesDashboard(ArrayList<SerializableDashboard> enemiesDashboard) {
         this.enemiesDashboard = enemiesDashboard;
+    }
+
+    public HashMap<SerializableDashboard, ArrayList<SerializableLeaderCard>> getEnemiesActivatedLeaderCards() {
+        return enemiesActivatedLeaderCards;
+    }
+
+    public void addEnemyLeaderCard(SerializableDashboard enemyDashboard, SerializableLeaderCard leaderCard){
+        enemiesActivatedLeaderCards.get(enemyDashboard).add(leaderCard);
     }
 }
