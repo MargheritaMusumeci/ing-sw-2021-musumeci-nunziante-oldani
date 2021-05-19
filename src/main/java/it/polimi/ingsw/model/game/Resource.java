d5869f4e91e6e2e2fb7fe167ee83841f81bd4000
matteoldani <matte.oldani99@gmail.com>
2021-05-19 13:49:42 +0200

    Added labels with the correct ascii code to display resources

--- src/main/java/it/polimi/ingsw/model/game/Resource.java
+++ src/main/java/it/polimi/ingsw/model/game/Resource.java
@@ -1,11 +1,17 @@
 package it.polimi.ingsw.model.game;
 
 public enum Resource {
-    COIN,
-    SHIELD,
-    ROCK,
-    SERVANT,
-    FAITH,
-    NOTHING,
-    WISH;
+    COIN("\u001b[33m●\u001b[0m"),
+    SHIELD("\u001b[34m●\u001b[0m"),
+    ROCK("\u001b[37m●\u001b[0m"),
+    SERVANT("\u001b[35m●\u001b[0m"),
+    FAITH("\u001b[31m●\u001b[0m"),
+    NOTHING("\u001b[0m●\u001b[0m"),
+    WISH("");
+
+    public final String label;
+
+    private Resource(String label) {
+        this.label = label;
+    }
 }
