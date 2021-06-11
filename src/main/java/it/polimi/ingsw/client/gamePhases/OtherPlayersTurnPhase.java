63d874a010cab83d24e086e28730ba917148085e
matteoldani <matte.oldani99@gmail.com>
2021-06-11 11:49:34 +0200

    Add print to say that is not your turn

--- src/main/java/it/polimi/ingsw/client/gamePhases/OtherPlayersTurnPhase.java
+++ src/main/java/it/polimi/ingsw/client/gamePhases/OtherPlayersTurnPhase.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.client.gamePhases;
 
 import it.polimi.ingsw.client.CLI.CLI;
+import it.polimi.ingsw.utils.Constants;
 
 public class OtherPlayersTurnPhase extends Phase{
     @Override
     public void makeAction(CLI cli) {
 
         synchronized (this){
+            System.out.println(Constants.ANSI_YELLOW + "It's not your turn!" +Constants.ANSI_RESET);
             try {
                 wait();
             } catch (InterruptedException e) {
