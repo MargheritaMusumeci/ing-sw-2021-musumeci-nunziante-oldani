235e4aeb419a8638155dca3f6ef9a0ab7320faf9
matteoldani <matte.oldani99@gmail.com>
2021-06-12 15:53:11 +0200

    Added main for client able to start a gui or a cli based on args

---
+++ src/main/java/it/polimi/ingsw/client/App.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.client.cli.CLI;
+import it.polimi.ingsw.client.gui.GUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        if(args.length == 0){
+            //GUI.main(null);
+            GUI gui = new GUI();
+            gui.launch(args);
+        }else{
+            if(args[0].equals("cli")){
+                new CLI();
+                //CLI.main(null);
+            }else{
+                //GUI.main(null);
+                GUI gui = new GUI();
+                gui.launch(args);
+            }
+        }
+
+    }
+}
