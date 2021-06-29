d5c5baa36d90416c66c8b786773243a1a14972cc
matteoldani <matte.oldani99@gmail.com>
2021-06-29 08:49:39 +0200

    handles persistence in cli

--- src/main/java/it/polimi/ingsw/client/messageHandler/MessageHandlerCLI.java
+++ src/main/java/it/polimi/ingsw/client/messageHandler/MessageHandlerCLI.java
@@ -2,11 +2,8 @@ package it.polimi.ingsw.client.messageHandler;
 
 import it.polimi.ingsw.client.cli.CLI;
 import it.polimi.ingsw.client.ClientSocket;
-import it.polimi.ingsw.client.cli.gamePhases.EndGamePhase;
-import it.polimi.ingsw.client.cli.gamePhases.InitialLeaderCardSelectionPhase;
-import it.polimi.ingsw.client.cli.gamePhases.InitialResourcesSelection;
+import it.polimi.ingsw.client.cli.gamePhases.*;
 import it.polimi.ingsw.client.cli.gamePhases.myTurnPhases.MyTurnPhase;
-import it.polimi.ingsw.client.cli.gamePhases.OtherPlayersTurnPhase;
 import it.polimi.ingsw.messages.sentByClient.ExitGameMessage;
 import it.polimi.ingsw.messages.sentByServer.*;
 import it.polimi.ingsw.messages.sentByServer.configurationMessagesServer.*;
@@ -119,7 +116,9 @@ public class MessageHandlerCLI extends MessageHandler{
 
     @Override
     public void handleMessage(PersistenceMessage message) {
-
+        cli.setGamePhase(new WaitingOtherPlayersPhase());
+        System.out.println(Constants.ANSI_GREEN + "Persistence is recreating your game" + Constants.ANSI_RESET);
+        new Thread(cli).start();
     }
 
     @Override
